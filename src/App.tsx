6bd30c2dccbec2500879efa9ae7814fcc33e3ada
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-09-24 11:04:05 -0400

    step 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import Form from "./components/Form"
 
 function App() {
 
   return (
     <>
-      <h1 className=" text-4xl">Contador Calorias</h1>
+      <header className="bg-lime-600 py-3">
+          <div className="max-w-4xl mx-auto flex justify-between">
+                <h1 className="text-center text-lg font-bold text-white uppercase">
+                  Calories Counter
+                </h1>
+          </div>
+      </header>
+
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          {/* Form here */}
+          <Form
+            
+          />
+        </div>
+      </section>
     </>
   )
 }
