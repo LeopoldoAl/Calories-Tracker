aa2cba0305dd4e667997fc17e651b499eec5c7b9
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-09-26 12:18:03 -0400

    step 19

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -17,7 +17,7 @@ export const activityReducer = (
 ) => {
     if (action.type === 'save-activity') {
         // This code works the logic for updating the State
-        console.log(action.payload.newActivity)
+        
         return {
             ...state,
             activities: [...state.activities, action.payload.newActivity]
