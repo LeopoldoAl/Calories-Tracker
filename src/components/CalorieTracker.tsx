93e38469a279a17055eabe1e87d3875d55333475
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-09-30 10:49:11 -0400

    step 35

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react"
 import type { Activity } from "../types"
+import CalorieDisplay from "./CalorieDisplay"
 
 type CalorieTrackerProps = {
     activities: Activity[]
@@ -10,19 +11,24 @@ export default function CalorieTracker({activities}: CalorieTrackerProps) {
     // Counters
     const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category  === 1 ? total + activity.calories : total ,0), [activities])
 
+    const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category  === 2 ? total + activity.calories : total ,0), [activities])
+
   return (
     <>
-        <h2 className="text-4xl font-black text-white text-center">
+        <h2 className="text-6xl font-black text-white text-center">
             Calories Resume
         </h2>
 
         <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
             
-            <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-
-                <span className="font-black text-4xl text-orange">{caloriesConsumed}</span>
-                Consumed
-            </p>
+            <CalorieDisplay
+                calories={caloriesConsumed}
+                text="Consumed"
+            />
+            <CalorieDisplay
+                calories={caloriesBurned}
+                text="Burned"
+            />
         </div>
 
         
